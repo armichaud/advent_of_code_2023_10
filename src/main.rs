594b17d43da988b8165349da2f69b76535b05e15
armichaud <email@alexandermichaud.tech>
2023-12-12 17:06:54 -0500

    Get matrix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use nalgebra::{DMatrix};
+use std::{fs::File, io::{BufReader, BufRead}};
+
+fn get_matrix(filename: &str) -> DMatrix<char> {
+    let file = File::open(filename);
+    let reader = BufReader::new(file.unwrap());
+    let rows: Vec<Vec<char>> = reader
+        .lines()
+        .map(|line| line.unwrap().chars().collect())
+        .collect();
+    let num_rows = rows.len();
+    let num_cols = rows[0].len();
+    DMatrix::from_row_slice(num_rows, num_cols, &rows.iter().flatten().copied().collect::<Vec<char>>())
+}
+
+
+fn solution(filename: &str) -> i32 {
+    let matrix = get_matrix(filename);
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(solution("example.txt"), 8);
+    assert_eq!(solution("input.txt"), 0);
 }

4e4e7bd04cdcbd7e9c0016e5cdbd4b77a610071a
armichaud <email@alexandermichaud.tech>
2023-12-12 17:25:32 -0500

    Set up enum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
 use nalgebra::{DMatrix};
 use std::{fs::File, io::{BufReader, BufRead}};
 
+type Tuple = (char, Direction);
+
+#[derive(PartialEq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn from_tuple(tuple: Tuple) -> Option<Direction> {
+        let (c, previous) = tuple;
+        match c {
+            '-' => Some(previous),
+            '|' => Some(previous),
+            'L' => Some(if previous == Direction::Down {Direction::Right} else {Direction::Up}),
+            'J' => Some(if previous == Direction::Down {Direction::Left} else {Direction::Up}),
+            '7' => Some(if previous == Direction::Up {Direction::Left} else {Direction::Down}),
+            'F' => Some(if previous == Direction::Up {Direction::Right} else {Direction::Down}),
+            _ => None,
+        }
+    }
+}
+
 fn get_matrix(filename: &str) -> DMatrix<char> {
     let file = File::open(filename);
     let reader = BufReader::new(file.unwrap());
@@ -13,9 +38,22 @@ fn get_matrix(filename: &str) -> DMatrix<char> {
     DMatrix::from_row_slice(num_rows, num_cols, &rows.iter().flatten().copied().collect::<Vec<char>>())
 }
 
+fn find_start(matrix: DMatrix<char>) -> Option<(usize, usize)> {
+    for i in 0..matrix.nrows() {
+        for j in 0..matrix.ncols() {
+            if matrix[(i, j)] == 'S' {
+                return Some((i, j));
+            }
+        }
+    }
+    None
+}
 
 fn solution(filename: &str) -> i32 {
     let matrix = get_matrix(filename);
+    let visited = DMatrix::from_element(matrix.nrows(), matrix.ncols(), 0);
+    let stack = Vec::<Tuple>::new();
+    let start = find_start(matrix).unwrap();
     0
 }
 

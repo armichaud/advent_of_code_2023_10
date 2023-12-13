9467f4f736efb26844c9d6d1f63244f0e629acde
armichaud <email@alexandermichaud.tech>
2023-12-12 20:15:31 -0500

    Get starting paths

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{fs::File, io::{BufReader, BufRead}};
 
 type Tuple = (char, Direction);
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum Direction {
     Up,
     Down,
@@ -51,7 +51,7 @@ fn get_matrix(filename: &str) -> DMatrix<char> {
     DMatrix::from_row_slice(num_rows, num_cols, &rows.iter().flatten().copied().collect::<Vec<char>>())
 }
 
-fn find_start(matrix: DMatrix<char>) -> Option<(usize, usize)> {
+fn find_start(matrix: &DMatrix<char>) -> Option<(usize, usize)> {
     for i in 0..matrix.nrows() {
         for j in 0..matrix.ncols() {
             if matrix[(i, j)] == 'S' {
@@ -62,12 +62,44 @@ fn find_start(matrix: DMatrix<char>) -> Option<(usize, usize)> {
     None
 }
 
+fn get_paths(matrix: &DMatrix<char>, start: (usize, usize)) -> Vec<Tuple> {
+    let mut paths: Vec<Tuple> = Vec::new();
+    let (start_row, start_col) = start;
+
+    if start_row > 0 {
+        let up = matrix[(start_row - 1, start_col)];
+        if TOP_CONNECTED_PIPES.contains(&up) {
+            paths.push((up, Direction::Up));
+        }
+    }
+    if start_col > 0 {
+        let left = matrix[(start_col - 1, start_row)];
+        if LEFT_CONNECTED_PIPES.contains(&left) {
+            paths.push((left, Direction::Left));
+        }
+    }
+    if start_row < matrix.nrows() - 1 {
+        let down = matrix[(start_row + 1, start_col)];
+        if BOTTOM_CONNECTED_PIPES.contains(&down) {
+            paths.push((down, Direction::Down));
+        }
+    }
+    if start_col < matrix.ncols() - 1 {
+        let right = matrix[(start_row, start_col + 1)];
+        if RIGHT_CONNECTED_PIPES.contains(&right) {
+            paths.push((right, Direction::Right));
+        }
+    }
+    paths
+}
+
 fn solution(filename: &str) -> i32 {
     let matrix = get_matrix(filename);
     let visited = DMatrix::from_element(matrix.nrows(), matrix.ncols(), 0);
-    let stack = Vec::<Tuple>::new();
-    let start = find_start(matrix).unwrap();
-    0 
+    let start = find_start(&matrix).unwrap();
+    let paths: Vec<Tuple> = get_paths(&matrix, start);
+    println!("{:?}", paths);
+    0
 }
 
 fn main() {

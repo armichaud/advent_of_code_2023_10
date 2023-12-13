ac28557e963a813aef8f3a5d221e25d92e0abffd
armichaud <email@alexandermichaud.tech>
2023-12-12 20:43:59 -0500

    Part 1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use nalgebra::{DMatrix};
+use nalgebra::DMatrix;
 use std::{fs::File, io::{BufReader, BufRead}};
 
-type Tuple = (char, Direction);
+struct Pipe { 
+    symbol: char, 
+    previous_direction: Direction, 
+    coords: (usize, usize)
+}
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 enum Direction {
     Up,
     Down,
@@ -25,15 +29,14 @@ const RIGHT_CONNECTED_PIPES: [char; 3] = [HORIZONTAL_PIPE, J_PIPE, SEVEN_PIPE];
 const LEFT_CONNECTED_PIPES: [char; 3] = [HORIZONTAL_PIPE, L_PIPE, F_PIPE];
 
 impl Direction {
-    fn from_tuple(tuple: Tuple) -> Option<Direction> {
-        let (c, previous) = tuple;
+    fn from_char_and_direction(c: char, previous: &Direction) -> Option<Direction> {
         match c {
-            HORIZONTAL_PIPE => Some(previous),
-            VERTICAL_PIPE => Some(previous),
-            L_PIPE => Some(if previous == Direction::Down {Direction::Right} else {Direction::Up}),
-            J_PIPE => Some(if previous == Direction::Down {Direction::Left} else {Direction::Up}),
-            SEVEN_PIPE => Some(if previous == Direction::Up {Direction::Left} else {Direction::Down}),
-            F_PIPE => Some(if previous == Direction::Up {Direction::Right} else {Direction::Down}),
+            HORIZONTAL_PIPE => Some(previous.clone()),
+            VERTICAL_PIPE => Some(previous.clone()),
+            L_PIPE => Some(if previous == &Direction::Down {Direction::Right} else {Direction::Up}),
+            J_PIPE => Some(if previous == &Direction::Down {Direction::Left} else {Direction::Up}),
+            SEVEN_PIPE => Some(if previous == &Direction::Up {Direction::Left} else {Direction::Down}),
+            F_PIPE => Some(if previous == &Direction::Up {Direction::Right} else {Direction::Down}),
             _ => None,
         }
     }
@@ -62,47 +65,82 @@ fn find_start(matrix: &DMatrix<char>) -> Option<(usize, usize)> {
     None
 }
 
-fn get_paths(matrix: &DMatrix<char>, start: (usize, usize)) -> Vec<Tuple> {
-    let mut paths: Vec<Tuple> = Vec::new();
+fn get_paths(matrix: &DMatrix<char>, start: (usize, usize)) -> Vec<Pipe> {
+    let mut paths: Vec<Pipe> = Vec::new();
     let (start_row, start_col) = start;
 
     if start_row > 0 {
-        let up = matrix[(start_row - 1, start_col)];
+        let coords = (start_row - 1, start_col);
+        let up = matrix[coords];
         if TOP_CONNECTED_PIPES.contains(&up) {
-            paths.push((up, Direction::Up));
+            paths.push(Pipe { symbol: up, previous_direction: Direction::Up, coords });
         }
     }
     if start_col > 0 {
-        let left = matrix[(start_col - 1, start_row)];
+        let coords = (start_row, start_col - 1);
+        let left = matrix[coords];
         if LEFT_CONNECTED_PIPES.contains(&left) {
-            paths.push((left, Direction::Left));
+            paths.push(Pipe { symbol: left, previous_direction: Direction::Left, coords });
         }
     }
     if start_row < matrix.nrows() - 1 {
-        let down = matrix[(start_row + 1, start_col)];
+        let coords = (start_row + 1, start_col);
+        let down = matrix[coords];
         if BOTTOM_CONNECTED_PIPES.contains(&down) {
-            paths.push((down, Direction::Down));
+            paths.push(Pipe { symbol: down, previous_direction: Direction::Down, coords });
         }
     }
     if start_col < matrix.ncols() - 1 {
-        let right = matrix[(start_row, start_col + 1)];
+        let coords = (start_row, start_col + 1);
+        let right = matrix[coords];
         if RIGHT_CONNECTED_PIPES.contains(&right) {
-            paths.push((right, Direction::Right));
+            paths.push(Pipe { symbol: right, previous_direction: Direction::Right, coords });
         }
     }
     paths
 }
 
+fn traverse(matrix: &DMatrix<char>, visited: &mut DMatrix<i32>, start: Pipe) -> DMatrix<i32> {
+    let mut current = start;
+    let mut count = 1;
+    while matrix[(current.coords.0, current.coords.1)] != START {
+        if visited[current.coords] == 0 || visited[current.coords] > count {
+            visited[current.coords] = count;
+        }
+        count += 1;
+        let new_direction = Direction::from_char_and_direction(current.symbol, &current.previous_direction).unwrap();
+        match new_direction {
+            Direction::Up => {
+                current.coords.0 -= 1;
+            },
+            Direction::Down => {
+                current.coords.0 += 1;
+            },
+            Direction::Left => {
+                current.coords.1 -= 1;
+            },
+            Direction::Right => {
+                current.coords.1 += 1;
+            },
+        }
+        current.previous_direction = new_direction;
+        current.symbol = matrix[current.coords];
+    }
+    visited.to_owned()
+}
+
 fn solution(filename: &str) -> i32 {
     let matrix = get_matrix(filename);
-    let visited = DMatrix::from_element(matrix.nrows(), matrix.ncols(), 0);
+    let mut visited = DMatrix::from_element(matrix.nrows(), matrix.ncols(), 0);
     let start = find_start(&matrix).unwrap();
-    let paths: Vec<Tuple> = get_paths(&matrix, start);
-    println!("{:?}", paths);
-    0
+    let starting_points: Vec<Pipe> = get_paths(&matrix, start);
+    for start in starting_points {
+        visited = traverse(&matrix, &mut visited, start);
+    }
+    visited.iter().max().unwrap().to_owned()
 }
 
 fn main() {
     assert_eq!(solution("example.txt"), 8);
-    assert_eq!(solution("input.txt"), 0);
+    assert_eq!(solution("input.txt"), 7107);
 }
